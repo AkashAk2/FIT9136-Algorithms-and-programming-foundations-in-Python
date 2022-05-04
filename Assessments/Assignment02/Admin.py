6ea01bba02326e5844606bbed8a9d6bb8776cb62
abal0041 <abal0041@student.monash.edu>
2022-05-05 02:20:44 +1000

    Completed extract student info

--- Assessments/Assignment02/Admin.py
+++ Assessments/Assignment02/Admin.py
@@ -9,11 +9,11 @@ class Admin(User):
     def __init__(self, user_id = -1, username = "", password = ""):
         super().__init__(user_id, username, password)
 
-    def register_admin(self, username, password):
+    def register_admin(self):
         print('register admin function is openned')
         registered = False
-        file_handle_admin = open('user_admin.txt', 'r+')
-        encrypted_pass = User.encryption(self,password)
+        file_handle_admin = open('./data/course_data/user_admin.txt', 'r+')
+        encrypted_pass = User.encryption(self,self.password)
         for line in file_handle_admin:
             print("file handle for is open", line)
             line_list = [line.split(';;;')]
@@ -23,18 +23,18 @@ class Admin(User):
                 print("each", each)
                 print(each[1])
                 print(each[2])
-                if username == each[1] and encrypted_pass == each[2]:
+                if self.username == each[1] and encrypted_pass == each[2]:
                     print("User is already registered as admin!")
                     registered = True
                     break
 
         if registered == False:
-            write_admin = open("user_admin.txt", "a+")
+            write_admin = open("./data/course_data/user_admin.txt", "a+")
             temp_userid = User.generate_unique_user_id(self)
             str_user_id = str(temp_userid)
             write_admin.write("\n")
             write_admin.write(str_user_id)
-            write_admin.write(";;;" + username + ";;;" + encrypted_pass)
+            write_admin.write(";;;" + self.username + ";;;" + encrypted_pass)
             write_admin.close()
 
         file_handle_admin.close()
@@ -51,38 +51,98 @@ class Admin(User):
                           r'.*?\"content_info\":\"([0-9.]*)'
                 extracted_data = re.findall(pattern, each)
 
-                # with open("./data/result/course.txt", "a+") as course_file:
-                course_file = open("./data/result/course.txt", "a+")
-                for line in extracted_data:
-                    num = 0
-                    data_list = list(line)
-                    list_order = [0,1,5,2,3,4,6]
-                    ordered_list = [data_list[i] for i in list_order]
-                    for item in ordered_list:
-                        course_file.write(item)
-                        if num < len(item):
-                            course_file.write(";;;")
-                            num += 1
-                    course_file.write("\n")
+                with open("./data/result/course.txt", "a+") as course_file:
+                    # course_file = open("./data/result/course.txt", "a+")
+                    for line in extracted_data:
+                        num = 0
+                        data_list = list(line)
+                        list_order = [0,1,5,2,3,4,6]
+                        ordered_list = [data_list[i] for i in list_order]
+                        for item in ordered_list:
+                            course_file.write(item)
+                            if num < len(item):
+                                course_file.write(";;;")
+                                num += 1
+                        course_file.write("\n")
+                # course_file.close()
 
 
 
     def extract_review_info(self):
-        folder_path = r"./data/review_data"
-        os.chdir(folder_path)
-        for file in os.listdir():
-            if file.endswith(".json"):
-                file_path = f"{folder_path}/{file}"
-                with open(file_path, 'r') as one_file:
-                    data = one_file.readlines()
+        folder_path = './data/review_data'
+        for file in os.listdir(folder_path):
+            if file.endswith('.json'):
+                file_path = os.path.join(folder_path, file)
+                course_id = os.path.basename(file).split('.')[0]
+                # print(file_name)
+                with open(file_path,'r') as f:
+                    data = f.readlines()
                     for each in data:
                         pattern = r'\"_class\": \"course_review\", \"id\":.([0-9]*)\, \"content\":\ \"(.*?)\",' \
                                 r'.*? \"rating\": (.*?)\,'
-                        extracted_data = re.findall(pattern, one_file)
-                        print(extracted_data)
+                        extracted_data = re.findall(pattern, each)
+
+                        with open('./data/result/review.txt','a+') as review_file:
+                            for line in extracted_data:
+                                num = 0
+                                data_list = list(line)
+                                for item in data_list:
+                                    review_file.write(item)
+                                    if num < len(data_list):
+                                        review_file.write(";;;")
+                                        num += 1
+                                review_file.write(course_id)
+                                review_file.write("\n")
+
 
     def extract_students_info(self):
-        pass
+        id_existing = False
+        folder_path = './data/review_data'
+        for file in os.listdir(folder_path):
+            if file.endswith('.json'):
+                file_path = os.path.join(folder_path, file)
+                course_id = os.path.basename(file).split('.')[0]
+                with open(file_path, 'r') as f:
+                    data = f.readlines()
+                    for each in data:
+                        # id_pattern = r'\"_class\": \"user\", \"id\":.*?\,'
+                        # extracted_id = re.search(id_pattern, each)
+                        # if str(extracted_id) == "None":
+                        #     {
+                        #         id_existing == False
+                        #     }
+                        # else:
+                        #     {
+                        #         id_existing == True
+                        #     }
+                        all_pattern = r'\"_class\": \"course_review\", \"id\": (.*?)\,' \
+                                      r'.*?\"_class\": \"user\", \"id\": (.*?)\,.*? \"title\": \"(.*?)\",' \
+                                      r'.*?\"image_50x50\": \"(.*?)\",.*?\"initials\": \"(.*?)\"'
+                        extracted_data = re.findall(all_pattern, each)
+
+                        with open('./data/result/user_student.txt','a+') as student_file:
+                            for line in extracted_data:
+                                num = 1
+                                data_list = list(line)
+                                list_order = [1, 2, 3, 4, 0]
+                                ordered_list = [data_list[i] for i in list_order]
+                                user_name_extracted = str(ordered_list[1])
+                                stud_user_name = user_name_extracted.lower().replace(" ", "_")
+                                extracted_user_id = str(ordered_list[0])
+                                extracted_user_title = str(ordered_list[3]).lower()
+                                stud_password = extracted_user_title + extracted_user_id + extracted_user_title
+                                encrypted_pass = User.encryption(self,stud_password)
+                                user_name_password = [stud_user_name, encrypted_pass]
+                                final_list = [ordered_list[0], user_name_password[0], user_name_password[1],
+                                              ordered_list[1], ordered_list[2], ordered_list[3], ordered_list[4]]
+                                # print(len(final_list))
+
+                                for item in final_list:
+                                    student_file.write(item)
+                                    if num < len(final_list):
+                                        student_file.write(";;;")
+                                        num += 1
+                                student_file.write("\n")
 
     def extract_instructor_info(self):
         pass
@@ -110,4 +170,4 @@ class Admin(User):
 
 admin1 = Admin()
 # admin1.register_admin('Admin3','AdminPass')
-admin1.extract_review_info()
+admin1.extract_students_info()
